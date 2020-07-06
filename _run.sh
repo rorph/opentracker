a4e1734969d7cb6fa544e9d0f616c1317ee04c18
Rodrigo Orph <rodrigo.orph@gmail.com>
2020-07-05 22:35:44 -0300

    meh

--- _run.sh
+++ _run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker container rm -f $(docker container ls -q -f name=opentracker --format="{{.ID}}")
-docker run -d --restart=always -v $(pwd)/opentracker.conf:/etc/opentracker/opentracker.conf --name opentracker -p 9696:9696 -p 9696:9696/udp -p 6969:6969/udp -p 6969:6969 lednerb/opentracker-docker
+docker run -d --restart=always -v $(pwd)/opentracker.conf:/etc/opentracker/opentracker.conf --name opentracker -p 9696:9696 -p 9696:9696/udp -p 6969:6969/udp -p 6969:6969 z3n666/opentracker
